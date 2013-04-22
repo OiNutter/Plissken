1e9bc53fd01736778c8f668062d3d8f42f696b4d
OiNutter <willmckenzie@oinutter.co.uk>
2013-04-22 18:29:31 +0100

    Added all houdini .c files to setup.py to ensure correct linking

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('plissken',
-                    sources = ['plissken.c'])
+plissken = Extension('plissken',
+                    sources = [
+                    			'lib/buffer.c',
+                    			'lib/houdini_href_e.c',
+                    			'lib/houdini_html_e.c',
+                    			'lib/houdini_html_u.c',
+                    			'lib/houdini_js_e.c',
+                    			'lib/houdini_js_u.c',
+                    			'lib/houdini_uri_e.c',
+                    			'lib/houdini_uri_u.c',
+                    			'lib/houdini_xml_e.c',
+                    			'plissken.c'
+                    		]
+                    	)
 
 setup (name = 'Plissken',
        version = '0.1',
        description = 'Python wrapper for the C Houdini escape library',
-       ext_modules = [module1])
+       ext_modules = [plissken])
