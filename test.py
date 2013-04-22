b076dd5bf2bcf8b440cd7cc3e049d0781f4aeea7
OiNutter <willmckenzie@oinutter.co.uk>
2013-04-22 18:29:53 +0100

    Added test script

---
+++ test.py
@@ -0,0 +1,23 @@
+import plissken
+
+print '##############################'
+print 'plissken.escape_html'
+
+print '&: ',plissken.escape_html('&')
+print '>: ',plissken.escape_html('>')
+print '<: ',plissken.escape_html('<')
+print '": ',plissken.escape_html('"')
+print '\': ',plissken.escape_html('\'')
+print '/: ',plissken.escape_html('/')
+
+print '##############################'
+print 'plissken.unescape_html'
+
+print '&amp;:  ',plissken.unescape_html('&amp;')
+print '&gt;:   ',plissken.unescape_html('&gt;')
+print '&lt;:   ',plissken.unescape_html('&lt;')
+print '&quot;: ',plissken.unescape_html('&quot;')
+print '&#39;:  ',plissken.unescape_html('&#39;')
+print '&#47;:  ',plissken.unescape_html('&#47;')
+
+print '##############################'
