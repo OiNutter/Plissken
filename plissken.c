a5d7c7daa8f3f27414aebf4f22ddad532b7d3b62
OiNutter <willmckenzie@oinutter.co.uk>
2013-04-23 10:59:12 +0100

    Added remaining escape functions
    Now includes escape_js, unescape_js,escape_xml,escape_href,escape_uri
    and unescape_uri

--- plissken.c
+++ plissken.c
@@ -1,10 +1,29 @@
 #include <Python.h>
 #include "lib/houdini.h"
 
+typedef int (*houdini_cb)(gh_buf *, const uint8_t *, size_t);
+
 /**
 * html_secure instance variable
 */
 static int g_html_secure = 1;
+
+static PyObject *
+plissken__generic_escape(houdini_cb do_escape,PyObject *args)
+{
+    const char *str;
+    gh_buf buf = GH_BUF_INIT;
+
+    if (!PyArg_ParseTuple(args, "s", &str))
+        return NULL;
+
+    if (do_escape(&buf, (const uint8_t *)str, sizeof(str))) {
+        PyObject *result = Py_BuildValue("s",buf.ptr);
+        return result;
+    }
+
+    return Py_BuildValue("s",str);
+}
 
 static PyObject *
 plissken_escape_html(PyObject *self, PyObject *args)
@@ -26,23 +45,54 @@ plissken_escape_html(PyObject *self, PyObject *args)
 static PyObject *
 plissken_unescape_html(PyObject *self, PyObject *args)
 {
-    const char *str;
-    gh_buf buf = GH_BUF_INIT;
+   return plissken__generic_escape(&houdini_unescape_html,args);
+}
 
-    if (!PyArg_ParseTuple(args, "s", &str))
-        return NULL;
+static PyObject *
+plissken_escape_js(PyObject *self, PyObject *args)
+{
+    return plissken__generic_escape(&houdini_escape_js,args);
+}
 
-    if (houdini_unescape_html(&buf, (const uint8_t *)str, sizeof(str))) {
-        PyObject *result = Py_BuildValue("s",buf.ptr);
-        return result;
-    }
+static PyObject *
+plissken_unescape_js(PyObject *self, PyObject *args)
+{
+   return plissken__generic_escape(&houdini_unescape_js,args);
+}
 
-    return Py_BuildValue("s",str);
+static PyObject *
+plissken_escape_uri(PyObject *self, PyObject *args)
+{
+   return plissken__generic_escape(&houdini_escape_uri,args);
+}
+
+static PyObject *
+plissken_unescape_uri(PyObject *self, PyObject *args)
+{
+   return plissken__generic_escape(&houdini_unescape_uri,args);
+}
+
+static PyObject *
+plissken_escape_xml(PyObject *self, PyObject *args)
+{
+   return plissken__generic_escape(&houdini_escape_xml,args);
+}
+
+static PyObject *
+plissken_escape_href(PyObject *self, PyObject *args)
+{
+   return plissken__generic_escape(&houdini_escape_href,args);
 }
 
 static PyMethodDef PlisskenMethods[] = {
     { "escape_html",  plissken_escape_html, METH_VARARGS, "Escape an HTML string." },
     { "unescape_html",  plissken_unescape_html, METH_VARARGS, "Unescape an HTML string." },
+    { "escape_js",  plissken_escape_js, METH_VARARGS, "Escape a JS string." },
+    { "unescape_js",  plissken_unescape_js, METH_VARARGS, "Unescape a JS string." },
+    { "escape_uri",  plissken_escape_uri, METH_VARARGS, "Escape a URI string." },
+    { "unescape_uri",  plissken_unescape_uri, METH_VARARGS, "Unescape a URI string." },
+    { "escape_xml",  plissken_escape_xml, METH_VARARGS, "Escape an XML string." },
+    { "escape_href",  plissken_escape_href, METH_VARARGS, "Escape an HREF string." },
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
